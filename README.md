a2a7d674e31e2ba6803327943035ff887681a46c
Stephanie Wagner <stephwag@stephwag.com>
2018-12-19 20:53:40 -0500

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# flaglang
 Maps flag emojis to languages
+
+#### Example
+
+```
+import flaglang
+
+# Get a language code using a flag emoji.
+flaglang.flag_to_lang('🇩🇪')
+=> 'de'
+
+# Get a flag emoji from a language code.
+flaglang.lang_to_flag('de')
+=> '🇩🇪'
+```
---
+++ flaglang/__init__.py
@@ -0,0 +1,271 @@
+FLAGS = {
+    b'\U0001f1e6\U0001f1e9' : 'ca',
+    b'\U0001f1e6\U0001f1ea' : 'ar',
+    b'\U0001f1e6\U0001f1eb' : 'fa',
+    b'\U0001f1e6\U0001f1ec' : 'en',
+    b'\U0001f1e6\U0001f1ee' : None, # anguilla
+    b'\U0001f1e6\U0001f1f1' : 'sq', 
+    b'\U0001f1e6\U0001f1f2' : 'hy',
+    b'\U0001f1e6\U0001f1f4' : 'pt',
+    b'\U0001f1e6\U0001f1f6' : None, # antarctica
+    b'\U0001f1e6\U0001f1f7' : 'es',
+    b'\U0001f1e6\U0001f1f8' : 'sm',
+    b'\U0001f1e6\U0001f1f9' : 'de',
+    b'\U0001f1e6\U0001f1fa' : 'en',
+    b'\U0001f1e6\U0001f1fc' : None, # aruba
+    b'\U0001f1e6\U0001f1fd' : None, # aland islands
+    b'\U0001f1e6\U0001f1ff' : 'az',
+    b'\U0001f1e7\U0001f1e6' : 'bs',
+    b'\U0001f1e7\U0001f1e7' : 'en',
+    b'\U0001f1e7\U0001f1e9' : None, # bangladesh
+    b'\U0001f1e7\U0001f1ea' : 'nl',
+    b'\U0001f1e7\U0001f1eb' : 'fr',
+    b'\U0001f1e7\U0001f1ec' : 'bg',
+    b'\U0001f1e7\U0001f1ed' : 'ar',
+    b'\U0001f1e7\U0001f1ee' : 'fr', # burundi (Kirundi and French)
+    b'\U0001f1e7\U0001f1ef' : 'fr',
+    b'\U0001f1e7\U0001f1f1' : None, # st barthelemy
+    b'\U0001f1e7\U0001f1f2' : None, # bermuda
+    b'\U0001f1e7\U0001f1f3' : 'ms',
+    b'\U0001f1e7\U0001f1f4' : 'es',
+    b'\U0001f1e7\U0001f1f6' : None, # caribbean netherlands
+    b'\U0001f1e7\U0001f1f7' : 'pt',
+    b'\U0001f1e7\U0001f1f8' : 'en',
+    b'\U0001f1e7\U0001f1f9' : None, # bhutan
+    b'\U0001f1e7\U0001f1fb' : None, # bouvet island
+    b'\U0001f1e7\U0001f1fc' : None, # botswana
+    b'\U0001f1e7\U0001f1fe' : 'be',
+    b'\U0001f1e7\U0001f1ff' : 'en',
+    b'\U0001f1e8\U0001f1e6' : 'en',
+    b'\U0001f1e8\U0001f1e8' : None, # cocos islands
+    b'\U0001f1e8\U0001f1e9' : 'fr', # congo kinshasa
+    b'\U0001f1e8\U0001f1eb' : 'fr', # central african republic
+    b'\U0001f1e8\U0001f1ec' : 'fr', # congo brazzaville
+    b'\U0001f1e8\U0001f1ed' : 'de',
+    b'\U0001f1e8\U0001f1ee' : 'ci', # cote divoire
+    b'\U0001f1e8\U0001f1f0' : None, # cook islands
+    b'\U0001f1e8\U0001f1f1' : 'es',
+    b'\U0001f1e8\U0001f1f2' : 'fr',
+    b'\U0001f1e8\U0001f1f3' : 'zh-tw',
+    b'\U0001f1e8\U0001f1f4' : 'es',
+    b'\U0001f1e8\U0001f1f5' : None, # clipperton island
+    b'\U0001f1e8\U0001f1f7' : 'es',
+    b'\U0001f1e8\U0001f1fa' : 'es',
+    b'\U0001f1e8\U0001f1fb' : 'pt',
+    b'\U0001f1e8\U0001f1fc' : None, # curacao
+    b'\U0001f1e8\U0001f1fd' : None, # christmas island
+    b'\U0001f1e8\U0001f1fe' : 'el',
+    b'\U0001f1e8\U0001f1ff' : 'cs',
+    b'\U0001f1e9\U0001f1ea' : 'de',
+    b'\U0001f1e9\U0001f1ec' : None, # diego garcia
+    b'\U0001f1e9\U0001f1ef' : 'fr', # djibouti (French and Arabic)
+    b'\U0001f1e9\U0001f1f0' : 'da',
+    b'\U0001f1e9\U0001f1f2' : 'en',
+    b'\U0001f1e9\U0001f1f4' : 'es',
+    b'\U0001f1e9\U0001f1ff' : 'ar',
+    b'\U0001f1ea\U0001f1e6' : None, # ceuta melilla
+    b'\U0001f1ea\U0001f1e8' : 'es',
+    b'\U0001f1ea\U0001f1ea' : 'et',
+    b'\U0001f1ea\U0001f1ec' : 'ar',
+    b'\U0001f1ea\U0001f1ed' : None, # western sahara
+    b'\U0001f1ea\U0001f1f7' : None, # eritrea
+    b'\U0001f1ea\U0001f1f8' : 'es',
+    b'\U0001f1ea\U0001f1f9' : 'am',
+    b'\U0001f1ea\U0001f1fa' : None, # european union
+    b'\U0001f1eb\U0001f1ee' : 'fi',
+    b'\U0001f1eb\U0001f1ef' : 'en',
+    b'\U0001f1eb\U0001f1f0' : None, # falkland islands
+    b'\U0001f1eb\U0001f1f2' : 'en',
+    b'\U0001f1eb\U0001f1f4' : None, # faroe islands
+    b'\U0001f1eb\U0001f1f7' : 'fr',
+    b'\U0001f1ec\U0001f1e6' : 'fr',
+    b'\U0001f1ec\U0001f1e7' : 'en',
+    b'\U0001f1ec\U0001f1e9' : 'en',
+    b'\U0001f1ec\U0001f1ea' : 'ka',
+    b'\U0001f1ec\U0001f1eb' : None, # french guiana
+    b'\U0001f1ec\U0001f1ec' : None, # guernsey
+    b'\U0001f1ec\U0001f1ed' : 'en',
+    b'\U0001f1ec\U0001f1ee' : None, # gibraltar
+    b'\U0001f1ec\U0001f1f1' : None, # greenland
+    b'\U0001f1ec\U0001f1f2' : 'en',
+    b'\U0001f1ec\U0001f1f3' : 'fr',
+    b'\U0001f1ec\U0001f1f5' : None, # guadeloupe
+    b'\U0001f1ec\U0001f1f6' : 'es',
+    b'\U0001f1ec\U0001f1f7' : 'el',
+    b'\U0001f1ec\U0001f1f8' : None, # south georgia south sandwich islands
+    b'\U0001f1ec\U0001f1f9' : 'es',
+    b'\U0001f1ec\U0001f1fa' : None, # guam
+    b'\U0001f1ec\U0001f1fc' : 'pt',
+    b'\U0001f1ec\U0001f1fe' : 'en',
+    b'\U0001f1ed\U0001f1f0' : None, # hong kong
+    b'\U0001f1ed\U0001f1f2' : None, # heard mcdonald islands
+    b'\U0001f1ed\U0001f1f3' : 'es',
+    b'\U0001f1ed\U0001f1f7' : 'hr',
+    b'\U0001f1ed\U0001f1f9' : 'ht',
+    b'\U0001f1ed\U0001f1fa' : 'hu',
+    b'\U0001f1ee\U0001f1e8' : None, # canary islands
+    b'\U0001f1ee\U0001f1e9' : 'id',
+    b'\U0001f1ee\U0001f1ea' : 'en',
+    b'\U0001f1ee\U0001f1f1' : 'iw',
+    b'\U0001f1ee\U0001f1f2' : None, # isle of man
+    b'\U0001f1ee\U0001f1f3' : 'hi',
+    b'\U0001f1ee\U0001f1f4' : None, # british indian ocean territory
+    b'\U0001f1ee\U0001f1f6' : 'ar',
+    b'\U0001f1ee\U0001f1f7' : 'fa',
+    b'\U0001f1ee\U0001f1f8' : 'is',
+    b'\U0001f1ee\U0001f1f9' : 'it',
+    b'\U0001f1ef\U0001f1ea' : None, # jersey
+    b'\U0001f1ef\U0001f1f2' : 'en',
+    b'\U0001f1ef\U0001f1f4' : 'ar',
+    b'\U0001f1ef\U0001f1f5' : 'ja',
+    b'\U0001f1f0\U0001f1ea' : 'en',
+    b'\U0001f1f0\U0001f1ec' : 'ky',
+    b'\U0001f1f0\U0001f1ed' : 'km',
+    b'\U0001f1f0\U0001f1ee' : 'en',
+    b'\U0001f1f0\U0001f1f2' : 'ar',
+    b'\U0001f1f0\U0001f1f3' : 'en',
+    b'\U0001f1f0\U0001f1f5' : 'ko',
+    b'\U0001f1f0\U0001f1f7' : 'ko',
+    b'\U0001f1f0\U0001f1fc' : 'ar',
+    b'\U0001f1f0\U0001f1fe' : None, # cayman islands
+    b'\U0001f1f0\U0001f1ff' : 'kk',
+    b'\U0001f1f1\U0001f1e6' : 'lo',
+    b'\U0001f1f1\U0001f1e7' : 'ar',
+    b'\U0001f1f1\U0001f1e8' : 'en',
+    b'\U0001f1f1\U0001f1ee' : 'de',
+    b'\U0001f1f1\U0001f1f0' : 'si',
+    b'\U0001f1f1\U0001f1f7' : 'en',
+    b'\U0001f1f1\U0001f1f8' : 'en',
+    b'\U0001f1f1\U0001f1f9' : 'lt',
+    b'\U0001f1f1\U0001f1fa' : 'lb',
+    b'\U0001f1f1\U0001f1fb' : 'lv',
+    b'\U0001f1f1\U0001f1fe' : 'ar',
+    b'\U0001f1f2\U0001f1e6' : 'ar',
+    b'\U0001f1f2\U0001f1e8' : 'fr',
+    b'\U0001f1f2\U0001f1e9' : None, # moldova
+    b'\U0001f1f2\U0001f1ea' : 'sr',
+    b'\U0001f1f2\U0001f1eb' : None, # st martin
+    b'\U0001f1f2\U0001f1ec' : 'mg',
+    b'\U0001f1f2\U0001f1ed' : None, # marshall islands
+    b'\U0001f1f2\U0001f1f0' : 'mk',
+    b'\U0001f1f2\U0001f1f1' : 'so',
+    b'\U0001f1f2\U0001f1f2' : 'my',
+    b'\U0001f1f2\U0001f1f3' : 'mn',
+    b'\U0001f1f2\U0001f1f4' : None, # macau
+    b'\U0001f1f2\U0001f1f5' : None, # northern mariana islands
+    b'\U0001f1f2\U0001f1f6' : None, # martinique
+    b'\U0001f1f2\U0001f1f7' : None, # mauritania
+    b'\U0001f1f2\U0001f1f8' : None, # montserrat
+    b'\U0001f1f2\U0001f1f9' : 'mt',
+    b'\U0001f1f2\U0001f1fa' : None, # mauritius
+    b'\U0001f1f2\U0001f1fb' : None, # maldives
+    b'\U0001f1f2\U0001f1fc' : 'ny',
+    b'\U0001f1f2\U0001f1fd' : 'es',
+    b'\U0001f1f2\U0001f1fe' : 'ms',
+    b'\U0001f1f2\U0001f1ff' : 'pt',
+    b'\U0001f1f3\U0001f1e6' : 'en', # namibia (english is official, but afrikaans is most widely spoken)
+    b'\U0001f1f3\U0001f1e8' : None, # new caledonia
+    b'\U0001f1f3\U0001f1ea' : 'fr',
+    b'\U0001f1f3\U0001f1eb' : None, # norfolk island
+    b'\U0001f1f3\U0001f1ec' : 'en',
+    b'\U0001f1f3\U0001f1ee' : 'es',
+    b'\U0001f1f3\U0001f1f1' : 'nl',
+    b'\U0001f1f3\U0001f1f4' : 'no',
+    b'\U0001f1f3\U0001f1f5' : 'ne',
+    b'\U0001f1f3\U0001f1f7' : None, # nauru
+    b'\U0001f1f3\U0001f1fa' : None, # niue
+    b'\U0001f1f3\U0001f1ff' : 'en',
+    b'\U0001f1f4\U0001f1f2' : 'ar',
+    b'\U0001f1f5\U0001f1e6' : 'es',
+    b'\U0001f1f5\U0001f1ea' : 'es',
+    b'\U0001f1f5\U0001f1eb' : None, # french polynesia
+    b'\U0001f1f5\U0001f1ec' : None, # papua new guinea
+    b'\U0001f1f5\U0001f1ed' : 'tl',
+    b'\U0001f1f5\U0001f1f0' : 'ur',
+    b'\U0001f1f5\U0001f1f1' : 'pl',
+    b'\U0001f1f5\U0001f1f2' : None, # st pierre miquelon
+    b'\U0001f1f5\U0001f1f3' : None, # pitcairn islands
+    b'\U0001f1f5\U0001f1f7' : None, # puerto rico
+    b'\U0001f1f5\U0001f1f8' : 'ar', # palestinian territories
+    b'\U0001f1f5\U0001f1f9' : 'pt',
+    b'\U0001f1f5\U0001f1fc' : None, # palau
+    b'\U0001f1f5\U0001f1fe' : 'es',
+    b'\U0001f1f6\U0001f1e6' : 'ar',
+    b'\U0001f1f7\U0001f1ea' : None, # reunion
+    b'\U0001f1f7\U0001f1f4' : 'ro',
+    b'\U0001f1f7\U0001f1f8' : 'sr',
+    b'\U0001f1f7\U0001f1fa' : 'ru',
+    b'\U0001f1f7\U0001f1fc' : None, # rwanda
+    b'\U0001f1f8\U0001f1e6' : 'ar',
+    b'\U0001f1f8\U0001f1e7' : 'en',
+    b'\U0001f1f8\U0001f1e8' : None, # seychelles
+    b'\U0001f1f8\U0001f1e9' : 'ar',
+    b'\U0001f1f8\U0001f1ea' : 'sv',
+    b'\U0001f1f8\U0001f1ec' : None, # singapore
+    b'\U0001f1f8\U0001f1ed' : None, # st helena
+    b'\U0001f1f8\U0001f1ee' : 'sl',
+    b'\U0001f1f8\U0001f1ef' : None, # svalbard jan mayen
+    b'\U0001f1f8\U0001f1f0' : 'sk',
+    b'\U0001f1f8\U0001f1f1' : 'en',
+    b'\U0001f1f8\U0001f1f2' : 'it',
+    b'\U0001f1f8\U0001f1f3' : 'fr',
+    b'\U0001f1f8\U0001f1f4' : 'so',
+    b'\U0001f1f8\U0001f1f7' : 'nl',
+    b'\U0001f1f8\U0001f1f8' : 'en',
+    b'\U0001f1f8\U0001f1f9' : 'pt',
+    b'\U0001f1f8\U0001f1fb' : 'es',
+    b'\U0001f1f8\U0001f1fd' : None, # sint maarten
+    b'\U0001f1f8\U0001f1fe' : 'ar',
+    b'\U0001f1f8\U0001f1ff' : 'en',
+    b'\U0001f1f9\U0001f1e6' : None, # tristan da cunha
+    b'\U0001f1f9\U0001f1e8' : None, # turks caicos islands
+    b'\U0001f1f9\U0001f1e9' : 'fr',
+    b'\U0001f1f9\U0001f1eb' : None, # french southern territories
+    b'\U0001f1f9\U0001f1ec' : 'fr',
+    b'\U0001f1f9\U0001f1ed' : 'th',
+    b'\U0001f1f9\U0001f1ef' : 'tg',
+    b'\U0001f1f9\U0001f1f0' : None, # tokelau
+    b'\U0001f1f9\U0001f1f1' : None, # timor leste
+    b'\U0001f1f9\U0001f1f2' : None, # turkmenistan
+    b'\U0001f1f9\U0001f1f3' : 'ar',
+    b'\U0001f1f9\U0001f1f4' : None, # tonga
+    b'\U0001f1f9\U0001f1f7' : 'tr',
+    b'\U0001f1f9\U0001f1f9' : 'en',
+    b'\U0001f1f9\U0001f1fb' : None, # tuvalu
+    b'\U0001f1f9\U0001f1fc' : 'zh-tw',
+    b'\U0001f1f9\U0001f1ff' : 'sw',
+    b'\U0001f1fa\U0001f1e6' : 'uk',
+    b'\U0001f1fa\U0001f1ec' : 'en',
+    b'\U0001f1fa\U0001f1f2' : None, # us outlying islands
+    b'\U0001f1fa\U0001f1f3' : None, # united nations (for documentation purposes)
+    b'\U0001f1fa\U0001f1f8' : 'en',
+    b'\U0001f1fa\U0001f1fe' : 'es',
+    b'\U0001f1fa\U0001f1ff' : 'uz',
+    b'\U0001f1fb\U0001f1e6' : 'it',
+    b'\U0001f1fb\U0001f1e8' : 'en',
+    b'\U0001f1fb\U0001f1ea' : 'es',
+    b'\U0001f1fb\U0001f1ec' : None, # british virgin islands
+    b'\U0001f1fb\U0001f1ee' : None, # us virgin islands
+    b'\U0001f1fb\U0001f1f3' : 'vi',
+    b'\U0001f1fb\U0001f1fa' : None, # vanuatu
+    b'\U0001f1fc\U0001f1eb' : None, # wallis futuna
+    b'\U0001f1fc\U0001f1f8' : 'sm',
+    b'\U0001f1fc\U0001f1f0' : 'sq',
+    b'\U0001f1fe\U0001f1ea' : 'ar',
+    b'\U0001f1fe\U0001f1f9' : None, # mayotte
+    b'\U0001f1ff\U0001f1e6' : 'zu',
+    b'\U0001f1ff\U0001f1f2' : 'en',
+    b'\U0001f1ff\U0001f1fc' : 'en',
+}
+
+FLAGS_INV = {v: k for k, v in FLAGS.items()}
+
+def flag_to_lang(text):
+    f = text.encode('unicode_escape')
+    if f in FLAGS: return FLAGS[f]
+    return None
+
+def lang_to_flag(text):
+    if text in FLAGS_INV: return FLAGS_INV[text]
+    return None
+
